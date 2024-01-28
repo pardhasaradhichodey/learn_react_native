9b13077e2c3c32e419e48e4409e83cafd2f6df76
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2024-01-27 20:18:27 -0800

    Content of littlelemon

--- littleLemon/App.js
+++ littleLemon/App.js
@@ -1,7 +1,7 @@
 import { View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-
+import WelcomeScreen from './components/WelcomeScreen';
 export default function App() {
   return (
     <View
@@ -11,7 +11,7 @@ export default function App() {
       }}>
       <LittleLemonHeader />
       <View style={{flex:0.85}}>
-
+        <WelcomeScreen />
       </View>
       <LittleLemonFooter />
     </View>
